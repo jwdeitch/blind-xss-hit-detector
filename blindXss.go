32bde97ddffb73f2b24b7364a1e6836863f3eed0
jwdeitch <jwdeitch@gmail.com>
2016-05-10 08:42:12 -0400

    sending emails

--- blindXss.go
+++ blindXss.go
@@ -25,16 +25,17 @@ func main() {
 	http.HandleFunc("/hackerone/xss", capture)
 	err := http.ListenAndServe(":9090", nil)
 	helpers.Check(err)
-
 }
 
 func capture(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This page is intended to detect blind XSS vulnerabilities in the spirit of your organizations bug bounty program.")
 	logVisit(r)
 	if val, ok := r.URL.Query()["ne"]; ok {
 		if (val[0] == "1") {
-			fmt.Println("yes")
+			return
 		}
 	}
+	sendEmail()
 }
 
 func logVisit(r *http.Request) {
@@ -49,4 +50,8 @@ func logVisit(r *http.Request) {
 		Method: r.Method,
 	}
 	fmt.Println(visitor)
+}
+
+func sendEmail() {
+
 }
